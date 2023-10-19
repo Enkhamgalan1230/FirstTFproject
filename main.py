842a4c309a502bb647a0ab402e59c3b2357e5333
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2023-10-19 14:53:52 +0100

    first step

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import tensorflow as tf
+import numpy as np
+import cv2
+import os
+import random
+import matplotlib.pyplot as plt
+import pickle
 
-print(tf.__version__)
+DIRECTORY = r'C:\Users\Entwan\Downloads\archive\dogscats\valid'
+CATEGORIES = ['cats', 'dogs']
+
+for category in CATEGORIES:
+    folder = os.path.join(DIRECTORY,category)
+    for img in os.listdir(folder):
+        img_path = os.path.join(folder, img)
+        img_arr = cv2.imread(img_path)
+        plt.imshow(img_arr)
+        break
+
